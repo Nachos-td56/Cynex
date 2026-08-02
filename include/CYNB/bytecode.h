0e1ff97ea6b534bdf3e33e73ca88c035373707b6
Nachos <avankimbleton270@gmail.com>
2026-08-02 18:51:34 -0400

    Update bytecode.h with new opcodes and functions

--- include/CYNB/bytecode.h
+++ include/CYNB/bytecode.h
@@ -1,32 +1,45 @@
-// CYNB/bytecode.h
+// bytecode.h
 #ifndef CYNEX_BYTECODE_H
 #define CYNEX_BYTECODE_H
 
-#include <stdint.h>      // for uint8_t and uint32_t
-#include "value.h"    // for Value type
+#include <stdint.h>
+#include "value.h"
 
-#define CYN_MAGIC     "CYNB"
-#define CYN_VERSION   0x02
+#define CYN_MAGIC   "CYNB"
+#define CYN_VERSION 0x03
 
 typedef enum {
-    OP_LOAD_CONST = 0x01,
-    OP_STORE_VAR = 0x02,
-    OP_LOAD_VAR = 0x03,
-    OP_CONCAT = 0x05,
-    OP_PRINT = 0x04,
+    OP_LOAD_CONST = 0xA7,
+    OP_STORE_VAR = 0x3C,
+    OP_LOAD_VAR = 0xE1,
+    OP_PRINT = 0x5B,
+    OP_CONCAT = 0x92,
+    OP_ADD = 0xD4,
+    OP_SUB = 0x0F,
+    OP_MUL = 0x6A,
+    OP_DIV = 0xC8,
+    OP_NEG = 0x17,
     OP_HALT = 0xFF
 } Opcode;
 
 typedef struct {
     uint8_t* code;
-    uint32_t code_size;
+    uint32_t  code_size;
+    uint32_t  code_cap;
 
     Value* constants;
-    uint32_t const_count;
+    uint32_t  const_count;
+    uint32_t  const_cap;
 } BytecodeChunk;
 
-int  load_cynb(const char* filename, BytecodeChunk* chunk);
-void free_cynb(BytecodeChunk* chunk);
-void run_cynb(const BytecodeChunk* chunk);
+void     chunk_init(BytecodeChunk* chunk);
+void     chunk_free(BytecodeChunk* chunk);
+void     chunk_write(BytecodeChunk* chunk, uint8_t byte);
+uint8_t  chunk_add_const(BytecodeChunk* chunk, Value v);
 
-#endif /* CYNEX_BYTECODE_H */
+int      load_cynb(const char* filename, BytecodeChunk* chunk);
+int      save_cynb(const char* filename, const BytecodeChunk* chunk);
+void     free_cynb(BytecodeChunk* chunk);
+void     run_cynb(const BytecodeChunk* chunk);
+
+#endif
